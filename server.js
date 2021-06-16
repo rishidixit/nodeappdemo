10000f41725ed231d9df9ef2242e315decb8e39a
RishiDixit <rishidixit95.rd@gmail.com>
2021-06-16 14:30:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Docker + ECS Farget App Deployment - version-10!!</h1> \n');
+  res.send('<h1 style="color:red;">Docker + ECS Farget App Deployment - version-2!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
